847f28fa83bfbad05bb5b28f1ff37f81f525f763
Administrator <Administrator@DEEP-20161028TZ>
2017-04-11 11:55:31 +0800

    少年来一发

--- Button/Button/FrmLzs.cs
+++ Button/Button/FrmLzs.cs
@@ -10,19 +10,11 @@ using System.Windows.Forms;
 
 namespace Button
 {
-    public partial class Form1 : Form
+    public partial class FrmLzs : Form
     {
-        public Form1()
+        public FrmLzs()
         {
             InitializeComponent();
-        }
-
-        private void button5_Click(object sender, EventArgs e)
-        {
-            FrmLzs f = new FrmLzs();
-            this.Hide();
-            f.ShowDialog();
-            this.Show();
         }
     }
 }
